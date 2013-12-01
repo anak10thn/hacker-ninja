4105da9584a739abb69947daf209418e10a02c03
anak10thn <linux@toroo.org>
2013-12-01 14:52:58 +0700

    ADD: inserting short algorithm

---
+++ short/inserting.js
@@ -0,0 +1,17 @@
+console.log('inserting short algorithm');
+var j = [3,1,6,4,8,2,9,18,20,36,12,25,15,43,13,16,14,12]
+var k = j.length;
+
+for (t = 1; t < k; t++ ){
+	var a = j[t];
+	var c = t;
+	while((c >= 0)){
+		if(a <= j[c-1]){
+			j[c] = j[c-1];
+			j[c-1] = a;
+		}
+		c--;
+	}
+}
+
+console.log("Done : "+j)
